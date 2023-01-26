518c7c43ccbbf9c02654b8dfe905f23c7996515f
Ivan Jijon <ivanjijon@autonomia.digital>
2023-01-25 23:49:19 -0500

    Dynamically fill fields with item's data

--- src/app/list/sorted/sorted.component.ts
+++ src/app/list/sorted/sorted.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/angular';
+import { Item, ListItem } from 'src/app/shared/models/list-item.model';
+import { ProgressionService } from 'src/app/shared/services/progression.service';
+import { TabService } from 'src/app/shared/services/tab.service';
 
 @Component({
   selector: 'app-sorted',
@@ -7,8 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SortedComponent implements OnInit {
 
-  constructor() { }
+  items: Item[] = []
+  currentTab = ""
 
-  ngOnInit() {}
+  constructor(private tabService: TabService, private progressionService: ProgressionService) { }
 
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    ev.detail.complete();
+  }
+
+  ngOnInit() {
+    this.tabService.getObservable().subscribe((tab) => {
+      this.currentTab = tab
+      this.items = this.progressionService.getProgression(this.currentTab)
+    })
+  }
 }
