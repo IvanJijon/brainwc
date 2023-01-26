79ea7d32a8212b0624c3c52e72d750136ac01040
Ivan Jijon <ivanjijon@autonomia.digital>
2023-01-25 23:47:48 -0500

    Title should follow the selected tab

--- src/app/progression/progression.page.ts
+++ src/app/progression/progression.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TabService } from '../shared/services/tab.service';
 
 @Component({
   selector: 'app-progression',
   templateUrl: 'progression.page.html',
   styleUrls: ['progression.page.scss']
 })
-export class ProgressionPage {
+export class ProgressionPage implements OnInit {
 
-  constructor() { }
+  title = ""
+  constructor(private tabService: TabService) { }
+
+  ngOnInit() {
+    this.tabService.getObservable().subscribe((tab) => this.title = tab.toUpperCase())
+  }
 
 }
