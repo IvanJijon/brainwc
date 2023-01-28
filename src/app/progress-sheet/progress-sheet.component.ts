923284215cb0044f81cd4824d4610f2b7385ce31
Ivan Jijon <ivanjijon@autonomia.digital>
2023-01-28 16:04:17 -0500

    Configuring Progress sheet component to be triggered by an element ID passed as input. The title of the modal is also passed as an input.

--- src/app/progress-sheet/progress-sheet.component.ts
+++ src/app/progress-sheet/progress-sheet.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progress-sheet',
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressSheetComponent implements OnInit {
 
+  @Input() modalTrigger: string = ""
+  @Input() itemTitle: string = "Item Title"
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
